a458e170421dc354a2368b1651625f120b125c39
nhz2 <nhz2@cornell.edu>
2025-03-28 14:56:57 -0400

    edits

--- src/PhiloxArrays.jl
+++ src/PhiloxArrays.jl
@@ -3,11 +3,10 @@ using Random123: philox
 using StaticArrays
 export RawPhiloxArray
 
-# Write your package code here.
-
 """
-Struct for generating N array of arrays of 4 UInt32 numbers,
-where N is a tuple with the dimensions of the array
+    RawPhiloxArray{N} <: AbstractArray{SVector{4,UInt32},N}
+
+`N`-dimensional random array with elements of type `SVector{4,UInt32}`.
 """
 struct RawPhiloxArray{N} <: AbstractArray{SVector{4,UInt32},N}
     size::NTuple{N,Int64}
@@ -15,16 +14,10 @@ struct RawPhiloxArray{N} <: AbstractArray{SVector{4,UInt32},N}
     ctr1::UInt64
 end
 
-"""
-Generate 4 UInt32 numbers
-"""
 function p(key::UInt64, ctr0::UInt64, ctr1::UInt64)::SVector{4,UInt32}
     philox((key%UInt32,(key>>32)%UInt32), (ctr0%UInt32,(ctr0>>32)%UInt32,ctr1%UInt32,(ctr1>>32)%UInt32), Val(10))
 end
 
-"""
-Define methods for struct
-"""
 Base.size(P::RawPhiloxArray) = P.size
 Base.IndexStyle(::Type{<:RawPhiloxArray}) = IndexLinear()
 Base.getindex(P::RawPhiloxArray, i::Integer) = p(P.key, i%UInt64, P.ctr1)
@@ -32,35 +25,35 @@ Base.getindex(P::RawPhiloxArray, i::Integer) = p(P.key, i%UInt64, P.ctr1)
 """
 Convert UInt32 into a float of type Ftype in [-1,1]
 """
-function uneg11(Ftype::Type{<:Real},in::UInt32)::Ftype
-    fma(Ftype(in%Int32),Ftype(2)^(-31),Ftype(2)^(-32))
+function uneg11(Ftype::Type{<:Real}, in::UInt32)::Ftype
+    fma(Ftype(in%Int32), Ftype(2)^(-31), Ftype(2)^(-32))
 end
 
 """
 Convert UInt64 into a float of type Ftype in [-1,1]
 """
-function uneg11(Ftype::Type{<:Real},in::UInt64)::Ftype
-    fma(Ftype(in%Int32),Ftype(2)^(-63),Ftype(2)^(-64))
+function uneg11(Ftype::Type{<:Real}, in::UInt64)::Ftype
+    fma(Ftype(in%Int64), Ftype(2)^(-63), Ftype(2)^(-64))
 end
 
 """
 Convert UInt32 into a float of type Ftype in (0,1]
 """
-function u01(Ftype::Type{<:Real},in::UInt32)::Ftype
-    fma(in,Ftype(2)^(-32),Ftype(2)^(-33))
+function u01(Ftype::Type{<:Real}, in::UInt32)::Ftype
+    fma(Ftype(in), Ftype(2)^(-32), Ftype(2)^(-33))
 end
 
 """
 Convert UInt64 into a float of type Ftype in (0,1]
 """
-function u01(Ftype::Type{<:Real},in::UInt64)::Ftype
-    fma(in,Ftype(2)^(-64),Ftype(2)^(-65))
+function u01(Ftype::Type{<:Real}, in::UInt64)::Ftype
+    fma(Ftype(in), Ftype(2)^(-64), Ftype(2)^(-65))
 end
 
 """
-Takes in two UInt32 philox numbers and outputs two normally distrubuted floats of type Ftype
+Takes in two uniformly distributed UInt32 and outputs two normally distributed floats of type Ftype.
 """
-function boxmuller(Ftype::Type{<:Real},u1::UInt32,u2::UInt32)::SVector{2,Ftype}
+function boxmuller(Ftype::Type{<:Real}, u1::T, u2::T)::SVector{2,Ftype} where {T <: Union{UInt32, UInt64}}
     sqrt(-2*log(u01(Ftype,u2))).*sincospi(uneg11(Ftype,u1))
 end
 
@@ -74,9 +67,6 @@ struct ComplexGaussianVectorField{N} <: AbstractArray{SVector{3,Complex{Float32}
     ctr1::UInt64
 end
 
-"""
-Define methods for struct
-"""
 Base.size(G::ComplexGaussianVectorField) = G.size
 Base.IndexStyle(::Type{<:ComplexGaussianVectorField}) = IndexLinear()
 
@@ -90,4 +80,3 @@ function Base.getindex(G::ComplexGaussianVectorField, i::Integer)::SVector{3,Com
 end
 
 end
-
